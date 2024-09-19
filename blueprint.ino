b445d10a97d256c8b96f25ed332548343ccf2ade
Shyvardi <56295870+Shyvardi@users.noreply.github.com>
2024-09-19 18:21:14 +0300

    Create blueprint.ino

---
+++ blueprint.ino
@@ -0,0 +1,82 @@
+#include <SoftwareSerial.h>
+#include <DFRobotDFPlayerMini.h>
+
+SoftwareSerial mySerial(D5, D6); // RX, TX
+DFRobotDFPlayerMini dfPlayer;
+
+const int busyPin = D1; // BUSY pin from DFPlayer Mini
+const int moisturePin = A0; // Analog pin connected to the capacitive sensor
+const int ledPin = LED_BUILTIN; // Internal LED pin
+
+const int dryValue = 780; // Value for completely dry
+const int wetValue = 350; // Value for wet
+
+const float moistureThreshold = 70.0; // Moisture percentage threshold for playing a file
+const uint64_t sleepTime = 60000000; // Deep sleep time (10 minutes in microseconds)
+
+void setup() {
+ 
+  mySerial.begin(9600);
+
+  pinMode(busyPin, INPUT); // Set BUSY pin as input
+  pinMode(ledPin, OUTPUT); // Set LED pin as output
+
+  if (!dfPlayer.begin(mySerial)) {
+    Serial.println("DFPlayer Mini not detected.");
+    while (true); // Stop execution if DFPlayer Mini is not detected
+  }
+
+ 
+  dfPlayer.volume(30);  // Set volume level (0 to 30)
+
+  
+
+  // Read the soil moisture sensor and check moisture level
+  float moisturePercentage = getMoisturePercentage();
+  
+
+  if (moisturePercentage < moistureThreshold) {
+    // Play a random file and enter deep sleep
+    playRandomFile();
+  } else {
+  
+    delay(100); // Allow serial to flush
+    ESP.deepSleep(sleepTime);
+  }
+}
+
+void loop() {
+  if (digitalRead(busyPin) == HIGH) {
+    // If BUSY pin is HIGH, enter deep sleep mode
+   
+    delay(100); // Allow serial to flush
+    ESP.deepSleep(sleepTime);
+  }
+}
+
+float getMoisturePercentage() {
+  int moistureSum = 0;
+  for (int i = 0; i < 5; i++) {
+    moistureSum += analogRead(moisturePin);
+    delay(200); // Small delay between readings (optional)
+  }
+
+  int moistureAverage = moistureSum / 5;
+  return (float)(dryValue - moistureAverage) / (dryValue - wetValue) * 100;
+}
+
+void playRandomFile() {
+  delay(1000);
+  unsigned long t = millis();
+  int randomFile = (t % 7) + 1; // Random number between 1 and 6
+  dfPlayer.play(randomFile);   // Play the random file
+  
+ 
+
+  while (digitalRead(busyPin) == LOW) {
+    // Wait until the file is finished
+    delay(100);
+  }
+
+
+}
